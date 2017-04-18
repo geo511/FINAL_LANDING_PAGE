2e80062148215a1bdef13c16f30a772d4b325192
George Juarez <jorgejuarezh@gmail.com>
2017-04-18 11:56:29 -0500

    Add files via upload

---
+++ hebrews_detail/js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+	
+	$('.tooltipped').tooltip({delay: 50});
+	
+	  $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+ 
